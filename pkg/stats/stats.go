c075ad0091698c7d2c9a3fb44b25ae14a57e29ce
Shamshod <Shamshod@go.alif.academy>
2021-04-01 20:34:14 +0500

    добавим тест

---
+++ pkg/stats/stats.go
@@ -0,0 +1,38 @@
+package stats
+
+import (
+	"github.com/FirdavsMF/bank1/pkg/types"
+	
+)
+
+//Avg расчитовает среднюю сумму платежа
+func Avg(payments []types.Payment) types.Money {
+	// countPayments := types.Money(len(payments))
+	sumPaymenys := types.Money(0)
+	indexPayments := types.Money(0)
+	for _, payment := range payments {
+		if payment.Status == types.StatusFail {
+			continue
+		}
+		moneyPayments := payment.Amount
+		sumPaymenys += moneyPayments
+		indexPayments++
+	}
+	return sumPaymenys / indexPayments
+}
+
+// TotalInCategory находит 	сумму покупок в определённой категории.
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	sumPaymenys := types.Money(0)
+	for _, payment := range payments {
+		if payment.Category != category {
+			continue
+		}
+		if payment.Status == types.StatusFail {
+			continue
+		}
+		moneyPayments := payment.Amount
+		sumPaymenys += moneyPayments
+	}
+	return sumPaymenys
+}
